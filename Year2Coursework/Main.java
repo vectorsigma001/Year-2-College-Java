9bc6439bf20832fd4a3b55468758b213ffcd46a2
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-01 09:18:03 -0700

    Update Main.java

--- Year2Coursework/Main.java
+++ Year2Coursework/Main.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.TextField;
@@ -25,6 +26,11 @@ class Main{
   JPanel studentPanel,regularPanel,dropoutPanel;
   //forstudent
   JLabel studentNamelb,dateOfBirthlb,courseName,enrollmentID,dateOfEnrollment,courseDuration,tuitionFee;
+
+
+  JComboBox<String> yearComboBox;
+  JComboBox<String> monthComboBox;
+  JComboBox<String> dayComboBox;
   public Main(){
     frame=new JFrame("college Coursework");
     Toolkit kit=Toolkit.getDefaultToolkit();
@@ -64,10 +70,26 @@ class Main{
     studentPanel.add(dateOfBirthlb);
     
     ArrayList year=new ArrayList<>();
-    String years={"Year","1980","1981", "1982", "1983", ";1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022","2023"};
+    String[] years={"Year","1980","1981", "1982", "1983", ";1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022","2023"};
+    for(String year1:years){
+        year.add(year1);
+    }
     ArrayList month=new ArrayList<>();
+    String[] months={"Month","January","February","March","April","May","June","July","August","September","October","November","December"};
+    for(String month1:months){
+        month.add(month1);
+    }
     ArrayList day=new ArrayList<>();
-    frame.add(studentPanel);
+    String[] days={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
+    for(String day1:days){
+        day.add(day1);
+    }
+    yearComboBox=new JComboBox<>();
+    yearComboBox.add(year);
+    monthComboBox=new JComboBox<>();
+    monthComboBox.add(month);
+    dayComboBox=new JComboBox<>();
+    dayComboBox.add(day);
     
 
 
