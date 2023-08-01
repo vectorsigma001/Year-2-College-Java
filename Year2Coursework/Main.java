e310f0361ace699afb6b63fe8e741f3588f88922
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-01 10:19:48 -0700

    Update Main.java

--- Year2Coursework/Main.java
+++ Year2Coursework/Main.java
@@ -25,12 +25,16 @@ class Main{
   JFrame frame;
   JPanel studentPanel,regularPanel,dropoutPanel;
   //forstudent
-  JLabel studentNamelb,dateOfBirthlb,courseName,enrollmentID,dateOfEnrollment,courseDuration,tuitionFee;
+  JLabel studentNamelb,dateOfBirthlb,courseNamelb,enrollmentIDlb,dateOfEnrollmentlb1,dateOfEnrollmentlb2,courseDurationlb,tuitionFeelb;
+  
 
+  JComboBox<String> yearDobComboBox;
+  JComboBox<String> monthDobComboBox;
+  JComboBox<String> dayDobComboBox;
 
-  JComboBox<String> yearComboBox;
-  JComboBox<String> monthComboBox;
-  JComboBox<String> dayComboBox;
+  JComboBox<String> yearDoeComboBox;
+  JComboBox<String> monthDoeCombox;
+  JComboBox<String> dayDoeComboBox;
   public Main(){
     frame=new JFrame("college Coursework");
     Toolkit kit=Toolkit.getDefaultToolkit();
@@ -57,11 +61,11 @@ class Main{
     studentNamelb.setFont(new Font("Arial",Font.BOLD,14));
     studentNamelb.setForeground(Color.WHITE);
     TextField studentNameTf=new TextField("Student Name");
-    studentNameTf.setBounds(150,80,120,20);
+    studentNameTf.setBounds(140,80,120,20);
     studentNameTf.setFont(new Font("Arial",Font.PLAIN,14));
     studentPanel.add(studentNamelb);
     studentPanel.add(studentNameTf);
-
+   
     //Date Of Birth
     dateOfBirthlb=new JLabel("Date Of Birth:");
     dateOfBirthlb.setBounds(10,120,100,20);
@@ -69,20 +73,26 @@ class Main{
     dateOfBirthlb.setForeground(Color.WHITE);
     studentPanel.add(dateOfBirthlb);
     
-    yearComboBox=new JComboBox<>();
-    yearComboBox.setBounds(10,120, 40, 20);
-    studentPanel.add(yearComboBox);
-    monthComboBox=new JComboBox<>();
-    dayComboBox=new JComboBox<>();
+    //combobox config 1 date of birth
+    yearDobComboBox=new JComboBox<>();
+    yearDobComboBox.setBounds(140,120, 60, 20);
+    studentPanel.add(yearDobComboBox);
+    monthDobComboBox=new JComboBox<>();
+    monthDobComboBox=new JComboBox<>();
+    monthDobComboBox.setBounds(210, 120, 70, 20);
+    studentPanel.add(monthDobComboBox);
+    dayDobComboBox=new JComboBox<>();
+    dayDobComboBox.setBounds(290,120,60,20);
+    studentPanel.add(dayDobComboBox);
     
     //config of all comboBox for date of birth
     ArrayList year=new ArrayList<>();
-    String[] years={"Year","1980","1981", "1982", "1983", ";1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022","2023"};
+    String[] years={"Year","1980","1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022","2023"};
     for(String year1:years){
         year.add(year1);
     }
     for(String year1:years){
-      yearComboBox.addItem(year1);
+      yearDobComboBox.addItem(year1);
     }
     ArrayList month=new ArrayList<>();
     String[] months={"Month","January","February","March","April","May","June","July","August","September","October","November","December"};
@@ -90,18 +100,64 @@ class Main{
         month.add(month1);
     }
     for(String month1:months){
-      monthComboBox.addItem(month1);
+      monthDobComboBox.addItem(month1);
     }
     ArrayList day=new ArrayList<>();
-    String[] days={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
+    String[] days={"Day","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
     for(String day1:days){
         day.add(day1);
     }
     for(String day1:days){
-      dayComboBox.addItem(day1);
+      dayDobComboBox.addItem(day1);
     }
-   
+
+    //StudentPanel courseName
+    courseNamelb=new JLabel("Course Name:");
+    courseNamelb.setFont(new Font("Arial",Font.BOLD,14));
+    courseNamelb.setForeground(Color.WHITE);
+    courseNamelb.setBounds(10, 160,100,20);
+    studentPanel.add(courseNamelb);
+    TextField courseNametf=new TextField("Course Name");
+    courseNametf.setFont(new Font("Arial",Font.PLAIN,14));
+    courseNametf.setBounds(140,160,100,20);
+    studentPanel.add(courseNametf);
     
+    //studentPanel enrollmentID
+    enrollmentIDlb=new JLabel("Enrollment Id:");
+    enrollmentIDlb.setFont(new Font("Arial",Font.BOLD,14));
+    enrollmentIDlb.setForeground(Color.WHITE);
+    enrollmentIDlb.setBounds(10,200,100,20);
+    studentPanel.add(enrollmentIDlb);
+    TextField enrollmentIdtf=new TextField("Enrollment Id");
+    enrollmentIdtf.setFont(new Font("Arial",Font.PLAIN,14));
+    enrollmentIdtf.setBounds(140,200,100,20);
+    studentPanel.add(enrollmentIdtf);
+
+    //studentPanel DateOfEnrollment
+     dateOfEnrollmentlb1=new JLabel("Date of");
+     dateOfEnrollmentlb1.setFont(new Font("Arial",Font.BOLD,14));
+     dateOfEnrollmentlb1.setForeground(Color.WHITE);
+     dateOfEnrollmentlb1.setBounds(10,240,100,20);
+     studentPanel.add(dateOfEnrollmentlb1);
+     dateOfEnrollmentlb2=new JLabel("Enrollment:");
+     dateOfEnrollmentlb2.setFont(new Font("Arial",Font.BOLD,14));
+     dateOfEnrollmentlb2.setForeground(Color.WHITE);
+     dateOfEnrollmentlb2.setBounds(10,260,100,20);
+     studentPanel.add(dateOfEnrollmentlb2);
+     
+     //combobox config 1 date of enrollment
+     yearDoeComboBox=new JComboBox<>();
+     for(String year1:years){
+      yearDoeComboBox.addItem(year1);
+    }
+    monthDoeCombox=new JComboBox<>();
+    for(String month1:months){
+      monthDoeComboBox.addItem(month1);
+    }
+    dayDoeComboBox=new JComboBox<>();
+    for(String day1:days){
+      dayDoeComboBox.addItem(day1);
+    }
 
 
 
@@ -112,7 +168,7 @@ class Main{
 
 
 
-
+    frame.add(studentPanel);
 
 
 
@@ -146,6 +202,7 @@ class Main{
     Main object=new Main();
   }
 }
+
 
 
 
