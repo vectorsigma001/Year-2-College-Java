88bf6475a3b7ef37bf237401c1bd5b6374f63ee4
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-01 09:23:59 -0700

    Update Main.java

--- Year2Coursework/Main.java
+++ Year2Coursework/Main.java
@@ -69,27 +69,35 @@ class Main{
     dateOfBirthlb.setForeground(Color.WHITE);
     studentPanel.add(dateOfBirthlb);
     
+    yearComboBox=new JComboBox<>();
+    monthComboBox=new JComboBox<>();
+    dayComboBox=new JComboBox<>();
+
     ArrayList year=new ArrayList<>();
     String[] years={"Year","1980","1981", "1982", "1983", ";1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022","2023"};
     for(String year1:years){
         year.add(year1);
+    }
+    for(String year1:years){
+      yearComboBox.add(year1);
     }
     ArrayList month=new ArrayList<>();
     String[] months={"Month","January","February","March","April","May","June","July","August","September","October","November","December"};
     for(String month1:months){
         month.add(month1);
     }
+    for(String month1:months){
+      monthComboBox.add(month1);
+    }
     ArrayList day=new ArrayList<>();
     String[] days={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30"};
     for(String day1:days){
         day.add(day1);
     }
-    yearComboBox=new JComboBox<>();
-    yearComboBox.add(year);
-    monthComboBox=new JComboBox<>();
-    monthComboBox.add(month);
-    dayComboBox=new JComboBox<>();
-    dayComboBox.add(day);
+    for(String day1:days){
+      dayComboBox.add(day1);
+    }
+   
     
 
 
@@ -135,8 +143,6 @@ class Main{
     Main object=new Main();
   }
 }
-
-
 
 
 
