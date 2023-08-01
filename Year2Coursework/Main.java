d5974641bca19a466fdf0e4e4e788c62b3ca1712
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-01 17:17:04 +0545

    Create Main.java

---
+++ Year2Coursework/Main.java
@@ -0,0 +1,26 @@
+//frame manipulaions Classes and packages
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+
+class Main{
+  JFrame frame;
+  public Main(){
+    frame=new JFrame("college Coursework");
+    Toolkit kit=Toolkit.getDefaultToolkit();
+    Dimension screenDimension=kit.getScreenSize();
+    int screenWidth=screenDimension.width;
+    int screenHeight=screenDimension.height;
+    frame.setSize(screenWidth/2,screenHeight/2);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  }
+  public static void main(String args[]){
+    Main object=new Main();
+  }
+}
