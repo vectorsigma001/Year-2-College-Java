dfb115b05d821cfffb36b0d4a953a74af844b649
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-08-01 04:58:28 -0700

    Update Main.java

--- Year2Coursework/Main.java
+++ Year2Coursework/Main.java
@@ -3,19 +3,46 @@ import javax.swing.JFrame;
 import java.awt.Toolkit;
 import java.awt.Dimension;
 
+//making it look beautiful 
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
 class Main{
   JFrame frame;
+  JPanel studentPanel,regularPanel,dropoutPanel;
   public Main(){
     frame=new JFrame("college Coursework");
     Toolkit kit=Toolkit.getDefaultToolkit();
     Dimension screenDimension=kit.getScreenSize();
     int screenWidth=screenDimension.width;
     int screenHeight=screenDimension.height;
-    frame.setSize(screenWidth,screenHeight);
+    frame.setSize(screenWidth,screenHeight );
+
+
+
+    //working with studentPanel
+    studentPanel=new JPanel();
+    studentPanel.setSize(400,600);
+    studentPanel.setBackground(Color.ORANGE);
+    studentPanel.setLocation(50,25);
+    frame.add(studentPanel);
+    
+    //working with regularPanel
+    regularPanel=new JPanel();
+    regularPanel.setSize(400,600);
+    regularPanel.setBackground(new Color(0,128,128));
+    regularPanel.setLocation(500,25);
+    frame.add(regularPanel);
+
+
+
+
+
+
+
     frame.setResizable(false);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
